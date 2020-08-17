b7696e22140cfe759cc62b05f4a840607ea41b4d
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-08-17 17:58:22 +0300

    create scoreboard object

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
-const Player = (name, letter) => {
-	const letter = letter;
-	const name = name;
+window.onload = () => {
+	const scoreBoard = () => {
+		let x = 0;
+		let o = 0;
+
+		const update = (winner) => {
+			if (winner == 'X') {
+				x += 1;
+				console.log(x);
+				document.getElementById('xScore').innerText = x;
+			} else {
+				o += 1;
+				console.log(o);
+				document.getElementById('oScore').innerText = o;
+			}
+		};
+		return { x, o, update };
+	};
+
+	let scores = scoreBoard();
+	scores.update('X');
+	scores.update('X');
+	scores.update('O');
+	scores.update('X');
+	scores.update('O');
+	scores.update('X');
+	scores.update('O');
 };
 
-const GameBoard = () => {
-	let gameBoard = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-};
+// scoreBoard.updateScore(winner);
+// const Player = (name, letter) => {
+// 	const letter = letter;
+// 	const name = name;
+// };
 
-const Game = () => {
-	let player1 = new Player(name, letter);
-	let player2 = new Player();
+// const GameBoard = () => {
+// 	let gameBoard = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+// };
 
-	function play(position) {
-		if (!turn(this.name)) {
-			return 'not your turn';
-		} else if (valid(position)) {
-			updateBoard(this.letter, position);
-		} else {
-			return 'please type a valid position';
-		}
-	}
+// const Game = () => {
+// 	let player1 = new Player(name, letter);
+// 	let player2 = new Player();
 
-	function updateBoard(letter, position) {
-		/*some action*/
-		if (isWinner()) {
-			/* end game */
-		} else {
-			nextMove();
-		}
-	}
+// 	function play(position) {
+// 		if (!turn(this.name)) {
+// 			return 'not your turn';
+// 		} else if (valid(position)) {
+// 			updateBoard(this.letter, position);
+// 		} else {
+// 			return 'please type a valid position';
+// 		}
+// 	}
 
-	function isWinner() {}
-};
+// 	function updateBoard(letter, position) {
+// 		/*some action*/
+// 		if (isWinner()) {
+// 			/* end game */
+// 		} else {
+// 			nextMove();
+// 		}
+// 	}
+
+// 	function isWinner() {}
+// };
